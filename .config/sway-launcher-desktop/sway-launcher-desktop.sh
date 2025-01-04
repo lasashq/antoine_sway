1a5e727069579e0a1092c96c58567bcc5fc99f58
antoine <yashnov.am@phystech.edu>
2025-01-04 15:14:47 +0300

    waybar config, nitch, menu, bspwm-like layout, etc.

---
+++ .config/sway-launcher-desktop/sway-launcher-desktop.sh
@@ -0,0 +1,5 @@
+# kitty --class menu -e sway-launcher-desktop
+#kitty --class menu -e sway-launcher-desktop >/dev/null 2>&1
+#exec kitty --class menu --single-instance -e sway-launcher-desktop >/dev/null 2>&1
+#exec kitty --class menu -e sway-launcher-desktop >/dev/null 2>&1
+kitty --class menu -e sway-launcher-desktop >/dev/null 2>&1 &
